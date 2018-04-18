09dd1528e638cb7d4bcaf0dad0455601bf9afb6c
arunthampi <arun.thampi@gmail.com>
2018-04-18 11:34:28 -0700

    Add sample ipfs app

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+
+	"github.com/ipfs/go-ipfs/commands"
+	"github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/core/corehttp"
+)
 
 func main() {
-	fmt.Printf("p2pubsub running")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	nd, err := core.NewNode(ctx, &core.BuildCfg{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cctx := commands.Context{
+		Online: true,
+		ConstructNode: func() (*core.IpfsNode, error) {
+			return nd, nil
+		},
+	}
+
+	list, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("listening on: ", list.Addr())
+
+	if err := corehttp.Serve(nd, list, corehttp.CommandsOption(cctx)); err != nil {
+		log.Fatal(err)
+	}
 }
